d71e7bd2fe49749bb08c98a73064eccd7cfde74e
52326125 <zxc5328032@gmail.com>
2021-09-24 15:46:36 +0800

    reocrd

---
+++ src/components/Bar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app-bar app>
+    <v-btn text @click="toHome">
+      <v-icon>mdi-home</v-icon>
+      <span>MMS</span>
+    </v-btn>
+    <v-spacer/>
+    <span>本週收益: {{benifit}}</span>
+  </v-app-bar>
+</template>
+<script>
+
+export default {
+  data(){
+    return{
+      benifit: 0
+    }
+  },
+  methods: {
+    toHome(){
+      if(this.$router.history.current.path !== '/'){
+        this.$router.push('/')
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
